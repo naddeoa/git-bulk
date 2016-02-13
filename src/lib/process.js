18b72b99d632b647a7ed8e18006a0a2aa19775b1
Anthony Naddeo <anthony.naddeo@gmail.com>
2016-02-13 14:48:30 -0800

    Documented nexe compiler workaround

--- src/lib/process.js
+++ src/lib/process.js
@@ -1,3 +1,10 @@
 const process = 'process';
 
+/*
+    This file exists because of the nexe compiler. Currently, it doesn't support
+    native modules and it requires that this workaround is used to omit them from
+    bundling during compilation. See the caveats section for the nexe repo.
+
+    https://github.com/jaredallard/nexe
+ */
 module.exports = require(process);
